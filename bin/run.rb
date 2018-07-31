69821a8d7f9f9e5742ac16076535756da34d5d31
github username <github email address>
2018-07-31 09:45:07 -0400

    added_menu_select_questions_test_to_run

--- bin/run.rb
+++ bin/run.rb
@@ -3,16 +3,38 @@ require_relative '../config/environment'
 roberto = Actor.find(1)
 natsuki = Producer.find(1)
 
-system "clear"
-
 prompt = TTY::Prompt.new
 
-name = prompt.ask('What is your name?', default: ENV['USER'])
+system "clear"
+name = prompt.ask("What is your name?")
 
 system "clear"
+gender = prompt.select("Please select gender identity.", %w(Male Female TransMale TransFemale Genderqueer Something_Else Prefer_Not_to_Answer))
 
-age_range = prompt.select("What is your age range?", %w(18-29 30-45 45+))
+system "clear"
+choices = %w(16-21 21-30 30-35 35-45 45-50 50-60 60+)
+age = prompt.multi_select("Age Range: please select all that apply.", choices)
 
-binding.pry
+system "clear"
+race = prompt.select("Please select race.", %w(Asian Black/African Caucasian Hispanic/Latinx Native_American Pacific_Islander Prefer_Not_to_Answer))
+
+system "clear"
+table = Terminal::Table.new :title => name do |t|
+  t << ["Gender Identity", gender]
+  t << :separator
+  t.add_row ["Age Range", age]
+  t.add_separator
+  t.add_row ["Race", race]
+end
+
+puts table
+
+# prompt.keypress("\nPress Any Key To Continue")
+
+prompt.select("\nOPTIONS") do |menu|
+  menu.choice "Previous Record", 1
+  menu.choice "Next Record", 2
+  menu.choice "Main Menu", 3
+end
 
 puts "HELLO WORLD"
